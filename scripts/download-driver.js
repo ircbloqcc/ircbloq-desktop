bf28338772e0da281e432e6aed5cd322d04ce553
ircbloqcc <baskarhari4@gmail.com>
2021-07-03 16:57:39 +0530

    dont change

--- scripts/download-driver.js
+++ scripts/download-driver.js
@@ -4,8 +4,8 @@ const path = require('path');
 const os = require('os');
 const fs = require('fs');
 
-const user = 'ircbloqcc';
-const repo = 'ircbloq-driver';
+const user = 'openblockcc';//dont change
+const repo = 'openblock-driver';
 const outputdir = path.join(__dirname, '../drivers');
 const leaveZipped = false;
 
